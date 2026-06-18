703368b540e2b029211e3480628d79fe8b4d1b1c
akuggbg06 <akuggbg09@gmail.com>
2026-06-18 12:27:48 +0700

    Delete api/owner.js

--- api/owner.js
+++
@@ -1,5 +0,0 @@
-// Sama seperti webhook.js, ini untuk backup
-// Tapi kita sudah pakai webhook.js sebagai main
-export default async function handler(req, res) {
-    return res.status(200).json({ status: 'ok', message: 'Gunakan /api/webhook untuk Telegram' });
-}
